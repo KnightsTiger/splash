abc5ab14a0648f4244b14b404b17b14bf66c3d4d
knight <tocaldera6734@gmail.com>
2018-12-21 16:03:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/knight/splash/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.knight.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,Home.class));
+    }
+}
